82812faddf0c7f4320c2b84cbdc6be04b59ab5bb
Solomon Cammue <s.cammue@gmail.com>
2025-09-01 15:13:21 -0500

    added the type writer

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -1,14 +1,15 @@
 // import Image from 'next/image'
+"use client"
 import React from 'react'
 import { Button } from './button-component'
+import { Typewriter } from 'react-simple-typewriter'
 
 export const HeroComponent = () => {
     return (
         <div className='w-full'>
             <div
                 className="img-wrapper w-full h-[753px] bg-cover bg-center"
-                style={{ backgroundImage: "url('/hero.jpg')" }}
-            >
+                style={{ backgroundImage: "url('/hero.jpg')" }}>
                 {/* backdrop */}
                 <div className="flex justify-center items-center w-full h-full bg-black/60">
                     {/* inner text wrapper */}
@@ -18,14 +19,26 @@ export const HeroComponent = () => {
                         {/* main text title */}
                         <div className="main-txt-wrap flex flex-col items-center">
                             <h1 className='text-6xl font-bold'>Discover, Host, and Sell Event</h1>
-                            <h1 className='text-6xl font-bold'>Tickets in Liberia with <span className='text-primary'>Confidence|</span></h1>
+                            <h1 className='text-6xl font-bold'>
+                                Tickets in Liberia with <span className='text-primary'>
+                                    <Typewriter
+                                        words={['Confidence', 'Ease', 'Security', 'Speed']}
+                                        loop={0}
+                                        cursor
+                                        cursorStyle='|'
+                                        typeSpeed={70}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000}
+                                    />
+                                </span>
+                            </h1>
                         </div>
                         {/*sub text title */}
                         <h3 className='wrap-break-word w-2/3 text-center text-slate-300 my-2'>Liberia&apos;s leading ticketing platform for events of all sizes. Plan with ease, reach your audience, and sell tickets online—fast, secure, and reliable.</h3>
 
                         <div className="btn-row flex gap-18 mt-20">
-                            <Button name='Start Your Event Now'/>
-                            <Button name='Find Events Near You'variant='primary' />
+                            <Button name='Start Your Event Now' />
+                            <Button name='Find Events Near You' variant='primary' />
                         </div>
                     </div>
                 </div>
