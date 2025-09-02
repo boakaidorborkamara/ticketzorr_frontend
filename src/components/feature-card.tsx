3e5cdf7c9c0af3d04d94ecf02e96cc9b5730a701
Solomon Cammue <s.cammue@gmail.com>
2025-09-01 19:25:17 -0500

    added a interface for feature card and fix the bug in feature section

--- src/components/feature-card.tsx
+++ src/components/feature-card.tsx
@@ -1,14 +1,20 @@
 import { CalendarPlus } from 'lucide-react';
 
 
-export function FeatureCard(title?: string, description?: string) {
+interface Props {
+    title?: string,
+    description?: string
+}
+
+
+export function FeatureCard ({title,description}:Props) {
   return (
         <div>
           <div className="bg-black text-amber-50 p-8 rounded-lg m-4 flex gap-4">
             <CalendarPlus/>
             <div>
               <h3 className="font-bold text-[24px]">{title}</h3>
-              <p className="text-[16px">Feature Description</p>
+              <p className="text-[16px]">{description}</p>
             </div>
           </div>
         </div>
--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -1,5 +1,5 @@
 import NextImage from "next/image";
-import { FeatureCard } from "./feature-card"; 
+import { FeatureCard } from "./feature-card";
 
 
 
@@ -14,27 +14,27 @@ export function FeaturesSection() {
 
   // feature list
   const featuresList: featuresType[] = [
-    
-    {title: "Event Management", description: "Set up and manage your events easily."},
-    {title: "Customizable Event Pages", description: "Create stunning event pages that reflect your brand and captivate your audience."},
+
+    { title: "Event Management", description: "Set up and manage your events easily." },
+    { title: "Customizable Event Pages", description: "Create stunning event pages that reflect your brand and captivate your audience." },
 
   ]
 
   return (
     <div className="p-24">
-        <h1 className="text-center text-4xl font-bold ">Everything You Need in a Simple Platform!</h1>
-        <p className="text-center text-[888484]">No setup fees. No hidden costs. Just better ticketing</p>
+      <h1 className="text-center text-4xl font-bold ">Everything You Need in a Simple Platform!</h1>
+      <p className="text-center text-[888484]">No setup fees. No hidden costs. Just better ticketing</p>
 
-        {/* Cards  */}
-        <div className="flex flex-wrap justify-center mt-12">
-          
-          {featuresList.map((feature, i)=>{
-            console.log(i);
-            return <FeatureCard key={i}/>
-          })}
-        </div>
+      {/* Cards  */}
+      <div className="flex flex-wrap justify-center mt-12">
 
-       
+        {featuresList.map((feature, i) => {
+          return <FeatureCard key={i} title={feature.title} description={feature.description} />
+
+        })}
+      </div>
+
+
     </div>
   );
 }
