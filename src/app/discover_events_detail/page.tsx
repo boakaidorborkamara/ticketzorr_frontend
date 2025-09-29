004be8bdfbf4bdd0e9cb5f16cb26f23ac23e8127
Solomon Cammue <s.cammue@gmail.com>
2025-09-29 08:30:17 -0500

    event detail page

---
+++ src/app/discover_events_detail/page.tsx
@@ -0,0 +1,9 @@
+import EventDetail from "@/components/event-detail";
+
+export default async function DiscoverEventsDetail() {
+  return (
+    <div className="w-screen min-h-screen">
+      <EventDetail />
+    </div>
+  );
+}
